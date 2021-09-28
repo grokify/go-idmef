5f70afd9fe46a6487dc60210755ed42457282af8
John Wang <johncwang@gmail.com>
2021-09-28 04:55:43 -0700

    testdata: teardrop-attack: add

---
+++ testdata/example_teardrop-attack.go
@@ -0,0 +1,113 @@
+package testdata
+
+import (
+	"time"
+
+	"github.com/grokify/go-idmef"
+)
+
+// SampleAlertTeardropAttack provides the sample file from
+// https://datatracker.ietf.org/doc/html/draft-ietf-idwg-idmef-xml#section-7.1.1
+func SampleAlertTeardropAttack() *idmef.Message {
+	dt, err := time.Parse(time.RFC3339, "2000-03-09T10:01:25.93464-05:00")
+	if err != nil {
+		panic(err)
+	}
+	msg := &idmef.Message{
+		XMLNSIDMEF: idmef.XMLNSIDMEFUrl,
+		Version:    idmef.XMLNSIDMEFVersion,
+		Alert: &idmef.Alert{
+			MessageId: "abc123456789",
+			Analyzer: idmef.Analyzer{
+				AnalyzerId: "hq-dmz-analyzer01",
+				Node: &idmef.Node{
+					Category: "dns",
+					Location: "Headquarters DMZ Network",
+					Name:     "analyzer01.example.com",
+				},
+			},
+			CreateTime: idmef.NewTime(dt),
+			Source: []idmef.Source{
+				{
+					Indent: "a1b2c3d4",
+					Node: &idmef.Node{
+						Indent:   "a1b2c3d4-001",
+						Category: "dns",
+						Name:     "badguy.example.net",
+						Address: &idmef.Address{
+							Indent:   "a1b2c3d4-002",
+							Category: "ipv4-net-mask",
+							Address:  "192.0.2.50",
+							Netmask:  "255.255.255.255",
+						},
+					},
+				},
+			},
+			Target: []idmef.Source{
+				{
+					Indent: "d1c2b3a4",
+					Node: &idmef.Node{
+						Indent:   "d1c2b3a4-001",
+						Category: "dns",
+						Address: &idmef.Address{
+							Category: "ipv4-addr-hex",
+							Address:  "0xde796f70",
+						},
+					},
+				},
+			},
+			Classification: idmef.Classification{
+				Text: "Teardrop detected",
+				Reference: &idmef.Reference{
+					Origin: "bugtraqid",
+					Name:   "124",
+					URL:    "http://www.securityfocus.com/bid/124",
+				},
+			},
+		},
+	}
+	return msg
+}
+
+/*
+
+<?xml version="1.0" encoding="UTF-8"?>
+<idmef:IDMEF-Message
+	xmlns:idmef="http://iana.org/idmef"
+                        version="1.0">
+	<idmef:Alert messageid="abc123456789">
+		<idmef:Analyzer analyzerid="hq-dmz-analyzer01">
+			<idmef:Node category="dns">
+				<idmef:location>Headquarters DMZ Network</idmef:location>
+				<idmef:name>analyzer01.example.com</idmef:name>
+			</idmef:Node>
+		</idmef:Analyzer>
+		<idmef:CreateTime ntpstamp="0xbc723b45.0xef449129">
+         2000-03-09T10:01:25.93464-05:00
+       </idmef:CreateTime>
+		<idmef:Source ident="a1b2c3d4">
+			<idmef:Node ident="a1b2c3d4-001" category="dns">
+				<idmef:name>badguy.example.net</idmef:name>
+				<idmef:Address ident="a1b2c3d4-002"
+                          category="ipv4-net-mask">
+					<idmef:address>192.0.2.50</idmef:address>
+					<idmef:netmask>255.255.255.255</idmef:netmask>
+				</idmef:Address>
+			</idmef:Node>
+		</idmef:Source>
+		<idmef:Target ident="d1c2b3a4">
+			<idmef:Node ident="d1c2b3a4-001" category="dns">
+				<idmef:Address category="ipv4-addr-hex">
+					<idmef:address>0xde796f70</idmef:address>
+				</idmef:Address>
+			</idmef:Node>
+		</idmef:Target>
+		<idmef:Classification text="Teardrop detected">
+			<idmef:Reference origin="bugtraqid">
+				<idmef:name>124</idmef:name>
+				<idmef:url>http://www.securityfocus.com/bid/124</idmef:url>
+			</idmef:Reference>
+		</idmef:Classification>
+	</idmef:Alert>
+</idmef:IDMEF-Message>
+*/
--- unmarshal/sample_test.go
+++ unmarshal/sample_test.go
@@ -4,14 +4,47 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/grokify/go-idmef"
 	"github.com/grokify/go-idmef/testdata"
 )
 
-const testFileXML = "../testdata/example_pingofdeath.xml"
+const (
+	testFileXML = "../testdata/example_pingofdeath.xml"
+)
 
 // TestSampleAlert ensures parse sample correct.
 func TestSampleAlert(t *testing.T) {
-	mGo := testdata.SampleAlert()
+	compare(t, testFileXML, testdata.SampleAlert)
+	compare(t, "../testdata/example_teardrop-attack.xml", testdata.SampleAlertTeardropAttack)
+	/*
+		mGo := testdata.SampleAlert()
+
+		mFile, err := ReadFile(testFileXML)
+		if err != nil {
+			t.Errorf("unmarshal.ParseFile(\"%s\") error[%s]",
+				testFileXML, err.Error())
+		}
+
+		xGo, err := mGo.Bytes("", "  ")
+		if err != nil {
+			t.Errorf("xml.Unmarshal Marshal error[%s]", err.Error())
+		}
+
+		xFile, err := mFile.Bytes("", "  ")
+		if err != nil {
+			t.Errorf("xml.Unmarshal Marshal error[%s]", err.Error())
+		}
+		if string(xGo) != string(xFile) {
+			fmt.Println(string(xGo))
+			fmt.Println(string(xFile))
+			t.Error("testdata.SampleAlert() does not match test file.")
+		}
+	*/
+
+}
+
+func compare(t *testing.T, testFileXML string, sampleMessage func() *idmef.Message) {
+	mGo := sampleMessage()
 
 	mFile, err := ReadFile(testFileXML)
 	if err != nil {
