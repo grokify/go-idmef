c0881ed08154e2504763d46dd0049b06c5ff01cc
John Wang <johncwang@gmail.com>
2021-09-28 22:05:21 -0700

    feat: constants: init

---
+++ constants.go
@@ -0,0 +1,11 @@
+package idmef
+
+const (
+	CategoryAddressIPV4 = "ipv4-addr"
+	CategoryOSDevice    = "os-device"
+	CategoryNIS         = "nis"
+
+	LocationConsole = "console"
+	LocationLocal   = "local"
+	ServiceFinger   = "finger"
+)
